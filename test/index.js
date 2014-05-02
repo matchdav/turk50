cd34fdb3e09bbca0e0f910292ef5a81336ff77ad
Matthew Davidson <matthew.christopher.davidson@gmail.com>
2014-05-01 17:19:38 -0700

    promisified the API.  A+ style.  turk.request(operationName,args).then(fn);

--- test/index.js
+++ test/index.js
@@ -1,8 +1,35 @@
-var Turk = require('..');
-var should = require('should');
-console.log(Turk);
+var Turk = require('..'),
+	should = require('should'),
+	config = require(process.cwd()+'/config'),
+	turk = new Turk(config);
+
 describe('Turk',function(){
 	it('should exist',function(){
 		should.exist(Turk);
+		should.exist(turk);
 	});
-})
+});
+
+describe('turk',function(){
+	before(function(done){
+		this.timeout(6000)
+		turk.on('ready',done);
+	});
+	it('should exist',function(){
+		should.exist(turk);
+	});
+	it('#request-SearchHITs',function(done){
+		turk.request('SearchHITs',function(err, res, xml){
+			res.should.exist;
+			done();
+		});
+	});
+	it('request-SearchHITs, promiseified',function(done){
+		this.timeout(5000);
+		turk.request('SearchHITs').then(function(err, res, xml){
+			console.log('done with ',arguments)
+			done();
+		});
+	});
+
+});
